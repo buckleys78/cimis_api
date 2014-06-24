761d7cc5fe5b20045410af3741d227dea175cf2a
Steve Buckley <buckleys78@gmail.com>
2014-06-24 15:39:14 -0700

    completed seeding db

--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -6,14 +6,22 @@ module Api
       respond_to :json
 
       def index
-        respond_with Station.select("station_nbr",
-                                    "temperatures.calendar_date",
-                                    "temperatures.daily_max",
-                                    "temperatures.daily_min")
-                            .joins(:temperatures)
-                            .where("station_nbr = ?", params[:station_nbr].to_s)
-                            .where("temperatures.calendar_date >= ?", params[:start_date].to_date)
-                            .where("temperatures.calendar_date <= ?", params[:end_date].to_date)
+        if params[:station_nbr]
+
+          respond_with Station.select("station_nbr",
+                                      "temperatures.calendar_date",
+                                      "temperatures.daily_max",
+                                      "temperatures.daily_min")
+                              .joins(:temperatures)
+                              .where("station_nbr = ?", params[:station_nbr].to_s)
+                              .where("temperatures.calendar_date >= ?", params[:start_date].to_date)
+                              .where("temperatures.calendar_date <= ?", params[:end_date].to_date)
+        else
+          respond_with Station.select("station_nbr",
+                                      "name",
+                                      "county",
+                                      "is_active")
+        end
       end
 
 private
