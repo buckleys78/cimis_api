076bd06a232549f3ab8acabc4e566dd67618ee78
Steve Buckley <buckleys78@gmail.com>
2014-06-23 19:08:07 -0700

    import stations functionality

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,25 @@
+require 'roo'
 class Station < ActiveRecord::Base
   has_many :temperatures
+
+  def self.import(file)
+    spreadsheet_columns = [:station_nbr, :name, :county, :is_active]
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      station = find_by_id(row["id"]) || new
+      station.attributes = row
+      station.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".csv" then Csv.new(file.path, nil, :ignore)
+    when ".xls" then Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
 end
