d8dbca1d741b22d0b185e7ed64417e09364c17b7
Steve Buckley <buckleys78@gmail.com>
2014-06-26 15:45:36 -0700

    revised daily update to occur at 3_30am

--- config/initializers/nightly_update_from_cimis.rb
+++ config/initializers/nightly_update_from_cimis.rb
@@ -1,14 +1,11 @@
 require 'rufus-scheduler'
-
+#
 #  The rufus-scheduler singleton
 #
 s = Rufus::Scheduler.singleton
 #
-# s.every '24h' do
-s.cron '30 11 * * *' do
-
+s.cron '30 3 * * *' do
+  # Update at 3:30am each day.
   Rails.logger.info "daily CIMIS update via rufus-scheduler, at #{Time.now}"
-  # Api::V1::StationsController.create_daily_temps_from_cimis
   Api::Station.create_daily_temperatures_from_cimis
-
 end
