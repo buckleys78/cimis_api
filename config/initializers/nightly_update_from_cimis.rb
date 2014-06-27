2c987862aef45f787d8be8203a8111712bb8710a
Steve Buckley <buckleys78@gmail.com>
2014-06-27 11:34:55 -0700

    wrapped test in IntegrationTest class

--- config/initializers/nightly_update_from_cimis.rb
+++ config/initializers/nightly_update_from_cimis.rb
@@ -4,7 +4,7 @@ require 'rufus-scheduler'
 #
 s = Rufus::Scheduler.singleton
 #
-s.cron '30 3 * * *' do
+s.cron '30 23 * * *' do
   # Update at 3:30am each day.
   Rails.logger.info "daily CIMIS update via rufus-scheduler, at #{Time.now}"
   Api::Station.create_daily_temperatures_from_cimis
--- test/features/cimis_api/can_get_data_from_cimis_test.rb
+++ test/features/cimis_api/can_get_data_from_cimis_test.rb
@@ -1,18 +1,21 @@
 require 'test_helper'
 
-describe "Testing nightly updates from CIMIS" do
-  before :each do
-    @station_nbrs = "229,230,231"
-  end
+class CimisTest < ActionDispatch::IntegrationTest
+  describe "Testing nightly updates from CIMIS" do
+    before :each do
+      @station_nbrs = "229,230,231"
+    end
 
-  it "will can add unique but not duplicate temperature records" do
-    original_count = Temperature.count
-    # add two records
-    Api::Station.create_daily_temperatures_from_cimis @station_nbrs
-    Temperature.count.must_equal original_count + 4
-    # try to add them again, should not make count increase.
-    Api::Station.create_daily_temperatures_from_cimis @station_nbrs
-    Temperature.count.must_equal original_count + 4
-  end
+    it "will can add unique but not duplicate temperature records" do
+      original_count = Temperature.count
 
+      # add four records
+      Api::Station.create_daily_temperatures_from_cimis @station_nbrs
+      Temperature.count.must_equal original_count + 4
+
+      # try to add them again, should not make count increase.
+      Api::Station.create_daily_temperatures_from_cimis @station_nbrs
+      Temperature.count.must_equal original_count + 4
+    end
+  end
 end
